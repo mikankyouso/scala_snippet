f3cdc8978849d0722d6f9f1c3d92e17c8f2e38e9
mikankyouso <mikankyouso@gmail.com>
2012-03-09 10:54:17 +0900

    a

--- src/main/scala/Foo.scala
+++ src/main/scala/Foo.scala
@@ -1,5 +1,14 @@
+
 object Foo {
+  import scalaz._
+  import Scalaz._
+
   def main(args: Array[String]): Unit = {
-    
+
+    val x = List(1, 2) |+| List(3)
+    println(x)
+
+    val s = implicitly[Semigroup[List[Int]]]
+    println(s)
   }
 }
