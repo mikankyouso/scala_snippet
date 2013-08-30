83c9b1670f8d8f37b2fc5ece3c420df296fdcda4
mikankyouso <mikankyouso@gmail.com>
2013-08-30 15:01:38 +0900

    a

--- src/main/scala/pki/Sign.scala
+++ src/main/scala/pki/Sign.scala
@@ -7,32 +7,43 @@ import java.security.PrivateKey
 
 object Sign {
   def main(args: Array[String]) {
-    val alias = ""
-    val pass = "".toCharArray()
-    val ksFile = """"""
+    val ksFile = args(0)
+    val pass = args(1).toCharArray()
+    val alias = args(2)
 
-    val ks = KeyStore.getInstance("JKS")
-    ks.load(new FileInputStream(ksFile), pass)
+    val keyStore = KeyStore.getInstance("JKS")
+    keyStore.load(new FileInputStream(ksFile), pass)
 
-    val s = Signature.getInstance("SHA1withRSA")
+    val signature = Signature.getInstance("SHA256withRSA")
 
     //val data = Array[Byte](1, 2, 3)
-    val data = Array.ofDim[Byte](1024 * 1024 * 1024)
-    println("署名開始")
-    s.initSign(ks.getKey(alias, pass).asInstanceOf[PrivateKey])
-    s.update(data)
-    val sign = s.sign()
-    println("署名終了")
+    val data = Array.ofDim[Byte](1024 * 1024)
+    val privateKey = keyStore.getKey(alias, pass).asInstanceOf[PrivateKey]
+    println(privateKey)
+    println(privateKey.getAlgorithm())
+    println(privateKey.getFormat())
 
+    println("署名開始")
+    signature.initSign(privateKey)
+    signature.update(data)
+    val sign = signature.sign()
     println(sign.length)
     println(sign.deep)
+    println("署名終了")
 
     //data(2) = 4
     //sign(10) = 1
+    val certificate = keyStore.getCertificate(alias)
+    println(certificate)
+    val publicKey = certificate.getPublicKey()
+    println(publicKey)
+    println(publicKey.getAlgorithm())
+    println(publicKey.getFormat())
+
     println("検証開始")
-    s.initVerify(ks.getCertificate(alias))
-    s.update(data)
-    println(s.verify(sign))
+    signature.initVerify(certificate)
+    signature.update(data)
+    println(signature.verify(sign))
     println("検証終了")
   }
 }
